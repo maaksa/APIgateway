e428c964308c75c0cbd805d87732dd4c931de6ff
maaksa <milosmilos@ymail.com>
2020-12-21 20:08:21 +0100

    Initial commit

---
+++ src/main/java/sk_microservices/APIgateway/ApIgatewayApplication.java
@@ -0,0 +1,17 @@
+package sk_microservices.APIgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ApIgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIgatewayApplication.class, args);
+	}
+
+}
